088c8cd6f0a1eeb1bdb5799525b8ed9b326b4062
CesarPaz-cloud <cesarpazfalcon1@gmail.com>
2023-12-22 02:20:10 -0500

    bd actualzada

--- src/main/resources/bd.sql
+++ src/main/resources/bd.sql
@@ -10,7 +10,7 @@ create table docente(
 iddocente int primary key auto_increment not null,
 nombre varchar(45),
 dni char(8),
-fechNacimiento date,
+fechnacimiento date,
 sueldo double,
 email varchar(45),
 sexo varchar(45),
@@ -20,8 +20,9 @@ constraint fk_docente foreign key (idcategoria) references categoria(idcategoria
 
 INSERT INTO categoria (nombre) VALUES ("Física"), ("Química"), ("Biología");
 
-INSERT INTO docente (nombre, dni, fechNacimiento, sueldo, email, sexo, idcategoria)
+INSERT INTO docente (nombre, dni, fechnacimiento, sueldo, email, sexo, idcategoria)
 VALUES ("María López", "25507032", "1990-02-02", 20000, "maria.lopez@email.com", "Femenino", 1),
 ("Pedro García", "65432109", "1980-03-03", 30000, "pedro.garcia@email.com", "Masculino", 2),
 ("Ana Fernández", "32109876", "1970-04-04", 40000, "ana.fernandez@email.com", "Femenino", 3);
 
+select * from docente;
